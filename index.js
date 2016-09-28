a9579de4614a73b18195a0f28c20057144affe9b
Chris Moesel <cmoesel@mitre.org>
2016-09-28 17:30:40 -0400

    Add eslint configuration and fix eslint errors
    Added a configuration for eslint, as well as lint and lint:fix tasks to the package.json.
    
    Fixed all of the eslint errors, which mainly consisted of:
    * using 2-space tabs
    * using single-quotes instead of double-quotes
    * missing semicolons

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const {importFromFilePath} = require('./lib/text/import')
-const {exportToSchemas} = require('./lib/schema/export')
+const {importFromFilePath} = require('./lib/text/import');
+const {exportToSchemas} = require('./lib/schema/export');
 
 if (process.argv.length < 3) {
-    console.log("Missing path to SHR definition file")
+  console.error('Missing path to SHR definition file');
 }
 
-const namespaces = importFromFilePath(process.argv[2])
-for (ns of namespaces) {
-    console.log(JSON.stringify(exportToSchemas(ns), null, "  "))
+const namespaces = importFromFilePath(process.argv[2]);
+for (let ns of namespaces) {
+  console.log(JSON.stringify(exportToSchemas(ns), null, '  '));
 }
