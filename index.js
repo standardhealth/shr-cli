36d9803d36ffcc6587cf9275fc11e67702f50280
Chris Moesel <cmoesel@mitre.org>
2016-09-28 14:06:31 -0400

    Refactor into organized import/export libs
    Refactor code so it is organized by type (text, schema) and function (import, export).  Take import code out of main index.js and put it into text import package.  Simplify index.js to simply call the importer and pass its output to the exporter.
    
    This refactoring sets us up for better unit testing as well.

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const {FileStream, CommonTokenStream} = require('antlr4/index');
-const {ParseTreeWalker} = require('antlr4/tree');
-const {SHRLexer} = require('./lib/parsers/SHRLexer');
-const {SHRParser} = require('./lib/parsers/SHRParser');
-const {SHR2JS} = require('./lib/shr2js');
-const {namespaceToSchemas} = require('./lib/schemaExport.js')
+const {importFromFilePath} = require('./lib/text/import')
+const {exportToSchemas} = require('./lib/schema/export')
 
 if (process.argv.length < 3) {
     console.log("Missing path to SHR definition file")
 }
 
-const inputFile = process.argv[2];
-const chars = new FileStream(inputFile);
-const lexer = new SHRLexer(chars);
-const tokens  = new CommonTokenStream(lexer);
-const parser = new SHRParser(tokens);
-parser.buildParseTrees = true;
-const tree = parser.shr();
-
-const walker = new ParseTreeWalker();
-const shr2js = new SHR2JS();
-walker.walk(shr2js, tree);
-
-for (ns of shr2js.namespaces()) {
-    console.log(JSON.stringify(namespaceToSchemas(ns), null, "  "))
+const namespaces = importFromFilePath(process.argv[2])
+for (ns of namespaces) {
+    console.log(JSON.stringify(exportToSchemas(ns), null, "  "))
 }
