bc1877e1918fd8a570b182a25c3f119a1b56f22b
Quinn <GQUINN@MITRE.ORG>
2016-11-23 13:12:53 -0500

    initial version of the hierarchy exporter

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mkdirp = require('mkdirp');
 const {importFromFilePath} = require('./lib/text/import');
 const {exportToSchemas} = require('./lib/schema/export');
 const {exportToStructureDefinitions} = require('./lib/structdef/export');
+const {exportToHierarchyJSON} = require('./lib/hierarchy/export');
+
 if (process.argv.length < 3) {
   console.error('Missing path to SHR definition folder or file');
 }
@@ -14,6 +16,11 @@ const outDir = process.argv.length == 4 ? process.argv[3] : './out';
 for (const err of errors) {
   console.error(`Import Error: ${err}`);
 }
+
+const hierarchyJSON = exportToHierarchyJSON(namespaces);
+const hierarchyPath = `${outDir}/hierarchy/hierarchy.json`;
+mkdirp.sync(hierarchyPath.substring(0, hierarchyPath.lastIndexOf('/')));
+fs.writeFileSync(hierarchyPath, JSON.stringify(hierarchyJSON, null, '  '));
 
 for (const schema of exportToSchemas(namespaces)) {
   const filePath = outDir + url.parse(schema.id).pathname;
