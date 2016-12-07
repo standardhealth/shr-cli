453460e38ff3c4fb803bd6c9d0cd666b606e892c
Chris Moesel <cmoesel@mitre.org>
2016-12-07 09:35:26 -0500

    Add new index_entries files for html/markdown
    In addition to the index that shows ALL data elements, create an index that only shows entries.  Also don't show empty namespaces in the index.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const url = require('url');
+//const url = require('url');
 const mkdirp = require('mkdirp');
 const {importFromFilePath} = require('./lib/text/import');
-const {exportToSchemas} = require('./lib/schema/export');
+//const {exportToSchemas} = require('./lib/schema/export');
 const {exportToMarkdown, exportToHTML} = require('./lib/markdown/export');
-const {exportToStructureDefinitions} = require('./lib/structdef/export');
-const {exportToHierarchyJSON} = require('./lib/hierarchy/export');
+//const {exportToStructureDefinitions} = require('./lib/structdef/export');
+//const {exportToHierarchyJSON} = require('./lib/hierarchy/export');
 
 if (process.argv.length < 3) {
   console.error('Missing path to SHR definition folder or file');
@@ -18,12 +18,12 @@ const outDir = process.argv.length == 4 ? process.argv[3] : './out';
 for (const err of errors) {
   console.error(`Import Error: ${err}`);
 }
-
+/* COMMENTING OUT SINCE HIERARCHY HASN'T BEEN UPDATED TO NEW MODELS
 const hierarchyJSON = exportToHierarchyJSON(namespaces);
 const hierarchyPath = `${outDir}/hierarchy/hierarchy.json`;
 mkdirp.sync(hierarchyPath.substring(0, hierarchyPath.lastIndexOf('/')));
 fs.writeFileSync(hierarchyPath, JSON.stringify(hierarchyJSON, null, '  '));
-
+*/
 const exportDoc = function(namespaces, format) {
   const basePath = path.join(outDir, format);
   mkdirp.sync(basePath);
@@ -42,6 +42,7 @@ const exportDoc = function(namespaces, format) {
     return;
   }
   fs.writeFileSync(path.join(basePath, `index.${ext}`), result.index);
+  fs.writeFileSync(path.join(basePath, `index_entries.${ext}`), result.indexEntries);
   for (const ns of Object.keys(result.namespaces)) {
     const nsPath = path.join(basePath, ...ns.split('.'));
     const nsFilePath = path.join(nsPath, `index.${ext}`);
@@ -57,6 +58,7 @@ const exportDoc = function(namespaces, format) {
 exportDoc(namespaces, 'markdown');
 exportDoc(namespaces, 'html');
 
+/* COMMENTING OUT SINCE SCHEMA AND STRUCTURE DEFINTIONS HAVEN'T BEEN UPDATED TO NEW MODELS
 for (const schema of exportToSchemas(namespaces)) {
   const filePath = outDir + url.parse(schema.id).pathname;
   mkdirp.sync(filePath.substring(0, filePath.lastIndexOf('/')));
@@ -68,3 +70,4 @@ for (const structdef of exportToStructureDefinitions(namespaces)) {
   mkdirp.sync(filePath.substring(0, filePath.lastIndexOf('/')));
   fs.writeFileSync(filePath, JSON.stringify(structdef, null, '  '));
 }
+*/
