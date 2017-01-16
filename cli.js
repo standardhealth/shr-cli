1f719377c7b19e8817c906bd9b10329843d17361
Chris Moesel <cmoesel@mitre.org>
2017-01-16 17:51:08 -0500

    Update to 4.0.0 models and tools (partial)
    Updated to 4.0.0 models and importer, expander, and md-export tools.  json-export tool is disabled until it is updated.

--- cli.js
+++ cli.js
@@ -3,38 +3,40 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const {importFromFilePath} = require('shr-text-import');
 const {expand} = require('shr-expand');
-const {exportToJSON} = require('shr-json-export');
+//const {exportToJSON} = require('shr-json-export');
 const {exportToMarkdown, exportToHTML} = require('shr-md-export');
 
 if (process.argv.length < 3) {
   console.error('Missing path to SHR definition folder or file');
 }
 
-const {namespaces, errors} = importFromFilePath(process.argv[2]);
+const {specifications, errors} = importFromFilePath(process.argv[2]);
 for (const err of errors) {
   console.error(`Import Error: ${err}`);
 }
-const expanded = expand(namespaces);
+const expanded = expand(specifications);
 for (const err of expanded.errors) {
   console.error(`Expansion Error: ${err}`);
 }
 const outDir = process.argv.length == 4 ? process.argv[3] : './out';
 
-const hierarchyJSON = exportToJSON(namespaces);
+/*
+const hierarchyJSON = exportToJSON(specifications);
 const hierarchyPath = `${outDir}/json/shr.json`;
 mkdirp.sync(hierarchyPath.substring(0, hierarchyPath.lastIndexOf('/')));
 fs.writeFileSync(hierarchyPath, JSON.stringify(hierarchyJSON, null, '  '));
+*/
 
-const exportDoc = function(namespaces, format) {
+const exportDoc = function(specifications, format) {
   const basePath = path.join(outDir, format);
   mkdirp.sync(basePath);
 
   var result, ext;
   if (format == 'markdown') {
-    result = exportToMarkdown(namespaces);
+    result = exportToMarkdown(specifications);
     ext = 'md';
   } else if (format == 'html') {
-    result = exportToHTML(namespaces);
+    result = exportToHTML(specifications);
     ext = 'html';
     // Copy over the CSS
     // fs.createReadStream('./lib/markdown/shr-github-markdown.css').pipe(fs.createWriteStream(path.join(basePath, 'shr-github-markdown.css')));
@@ -56,5 +58,5 @@ const exportDoc = function(namespaces, format) {
   }
 };
 
-exportDoc(expanded.namespaces, 'markdown');
-exportDoc(expanded.namespaces, 'html');
+exportDoc(expanded.specifications, 'markdown');
+exportDoc(expanded.specifications, 'html');
