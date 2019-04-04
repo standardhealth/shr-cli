a1877369932fd319225c2e398c0b1a7116c1ed1f
garypgryan <gpgryan@gmail.com>
2019-04-04 08:45:38 -0400

    recurse into elements

--- print_elements.js
+++ print_elements.js
@@ -3,13 +3,9 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const { Identifier } = require('shr-models');
 
-const chalk = require('chalk');   //library for colorizing Strings
-const redColor = chalk.bold.redBright;
-const greenColor = chalk.bold.greenBright;
-
 function camelCaseToHumanReadable( instring ) {
 // Whenever you have a Capital letter, insert a space
-// Whenever you have a period, replace with ' - '
+// Whenever you have a period, replace with ' '
   let res = '';
   for (let letter of instring) {
     if (letter === '.') {
@@ -28,6 +24,30 @@ function camelCaseToHumanReadable( instring ) {
 
 function removeValuePrefix(instring) {
   return( instring.replace(/Value: /,''));
+}
+
+function  getDescrip(f, specs, de) {
+  let descrip = '';
+  if (f !== undefined && f !== null) {
+    descrip = f.description;
+    descrip = getDescription( specs, f );
+    if (descrip === undefined || descrip === null) {
+      descrip = de.description;
+    }
+    else {
+      descrip = descrip.replace(/["]+/g,'');
+    }
+  }
+  return(descrip);
+}
+
+function getCard(f) {
+  let card = ' ';
+  if ( f !== undefined && f !== null && f.effectiveCard !== undefined && f.effectiveCard !== null
+    && f.effectiveCard.toString() !== undefined && f.effectiveCard.toString() !== null) {
+    card = f.effectiveCard.toString();
+  }
+  return(card);
 }
 
 module.exports = function printElements(specs, config, out) {
@@ -52,97 +72,128 @@ module.exports = function printElements(specs, config, out) {
 
 function printElementsInNamespace(specs, namespace, out, lines) {
   const dataElements = specs.dataElements.byNamespace(namespace).sort((a, b) => a.identifier.name < b.identifier.name ? -1 : 1);
-  //console.log('**** dataElements=' + JSON.stringify(dataElements) + '\n\n\n\n');
-  let myContentProfiles = specs.contentProfiles;
 
   for (const de of dataElements) {
     let parent_element = de.identifier.name;
-    
-    const myRegex = /BodySite/i;
-    const myRegex2 = /^Observation/i;
-    if (myRegex2.test(de.identifier.name)  )  {
-    //  if (myRegex2.test(`{${de.identifier.name}.${name}`)  )  
-      //console.log('************* de.identifier.name=' + de.identifier.name );
-    }
-    let ms = 'Must Support';
-    //lines.push(`"${namespace}","${parent_element}","${de.identifier.name}",,"","","${de.description}"`);
+    let ms = ' ';
     let humanRead1 = camelCaseToHumanReadable(`${de.identifier.name}`);
     lines.push(`"${namespace}","${parent_element}","${de.identifier.name}","${humanRead1}","${de.description}","","","","${ms}"`);
     const deFieldLines = [];
+
     const valueAndFields = [de.value, ...de.fields];
     for (let i=0; i < valueAndFields.length; i++) {
-      const f = valueAndFields[i];
-      let name = i>0 ? f.identifier.name : 'Value';
-      if (name === undefined || name === null) {
-        name = ' ';
-      }
-      let card = ' ';
-      if ( f !== undefined && f !== null && f.effectiveCard !== undefined && f.effectiveCard !== null
-        && f.effectiveCard.toString() !== undefined && f.effectiveCard.toString() !== null) {
-        card = f.effectiveCard.toString();
-      }
-      const fValueAndPath = dive(specs, f);
-      //const dataType = getDataType(fValueAndPath);
-      let dataType = getDataType(fValueAndPath);
-      dataType = removeValuePrefix(dataType);
-      const valueSet = getValueSet(f, fValueAndPath);
-      let descrip = '';
-      if (f !== undefined && f !== null) {
-        descrip = f.description;
-        descrip = getDescription( specs, f );
-        
-        if (descrip === undefined || descrip === null) {
-          descrip = de.description;
-        }
-        else {
-          descrip = descrip.replace(/["]+/g,'');
+      let f = valueAndFields[i];
+      //console.log('\n ###f=' + JSON.stringify(f));
+      if (f !== undefined) {
+        let nestedDE = specs._dataElements.findByIdentifier( f.identifier );
+        let depth = 1;
+        //console.log('\n ***element=' +  `"${namespace}","${parent_element}","${de.identifier.name}"` );
+        if( nestedDE !== undefined && nestedDE !== null) {
+          expandLevel( specs, namespace, nestedDE, deFieldLines, depth, parent_element ) ;
         }
       }
-      //console.log(`'iden=' "${de.identifier.name}.${name}"`);
-      //const myRule = specs._contentProfiles.findRuleByIdentifierAndPath(name, de.identifier.name);
-      /*
-      if (de.identifier.name === 'MophologyBehavior') {
-        console.log('****** found MophologyBehavior');
-      }
-      const myRule = specs._contentProfiles.findRulesByIdentifierAndField(de.identifier.name, name) ;
-      if (myRule !== undefined && myRule.length > 0 ) {
-        console.log('rule=' + JSON.stringify(myRule));
-      }
-      const myRule2 = specs._contentProfiles.findByIdentifier(de.identifier) ;
-      if (myRule2 !== undefined  ) {
-        console.log('identifier='  + de.identifier + ' de.identifier.name=' +  de.identifier.name + ' rule=' + JSON.stringify(myRule2) );
-        let mustSupportStr = JSON.stringify(myRule2.identifier.fqn);
-        let mustSupportRulesStr = JSON.stringify(myRule2.rules);
-        console.log('mustSupportStr=' + mustSupportStr  + ' mustSupportRulesStr=' + mustSupportRulesStr );
-
-        for (const ruleInst of myRule2.rules) {
-          let fqn  = '';
-          console.log('ruleInst= '  + JSON.stringify(ruleInst) );
-          console.log('ruleInst.mustSupport= '  + JSON.stringify(ruleInst.mustSupport) );
-          console.log('ruleInst._path= '  + JSON.stringify(ruleInst._path) );
-          for (const pathInst of ruleInst._path) {
-            fqn = fqn + '.' + pathInst._name;
-            console.log(' pathInst._name=' +  pathInst._name);
-          }
-          console.log( 'fqn=' + fqn);
-        }
-      }
-      */
-      //deFieldLines.push(`"${namespace}","${parent_element}","${de.identifier.name}.${name}",${card},"${dataType}","${valueSet}","${descrip}"`);
-      //deFieldLines.push(`"${namespace}","${parent_element}","${de.identifier.name}.${name}","${descrip}",${card},"${dataType}","${valueSet}"`);
-      const humanRead = camelCaseToHumanReadable(`${de.identifier.name}.${name}`);     
-      deFieldLines.push(`"${namespace}","${parent_element}","${de.identifier.name}.${name}","${humanRead}","${descrip}",${card},"${dataType}","${valueSet}","${ms}"`);
     }
+
+    let f = de.value;
+    let name = '';
+    let card = getCard(f);
+    const fValueAndPath = dive(specs, f);
+    let dataType = getDataType(fValueAndPath);
+    dataType = removeValuePrefix(dataType);
+    let valueSet = getValueSet(f, fValueAndPath);
+
+    //if ( f !== undefined && f !== null && f.effectiveCard !== undefined && f.effectiveCard !== null
+    //  && f.effectiveCard.toString() !== undefined && f.effectiveCard.toString() !== null) {
+    //  card = f.effectiveCard.toString();
+    // }
+
+    let descrip = getDescrip(f, specs, de);
+    const humanRead = camelCaseToHumanReadable(`${de.identifier.name}.${name}`); 
+    //deFieldLines.push(`"${namespace}","${parent_element}","${de.identifier.name}.${name}","${humanRead}","${descrip}",${card},"${dataType}","${valueSet}","${ms}"`);
+
     deFieldLines.sort((a, b) => {
       if (a.startsWith(`${de.identifier.name}.Value,`)) return -1;
       else if (b.startsWith(`${de.identifier.name}.Value,`)) return 1;
       else if (a < b) return -1;
       else return 1;
     });
-    //lines.push(...deFieldLines, '');
     lines.push( ...deFieldLines );
   }
   fs.writeFileSync(path.join(out, `elements_${namespace.replace(/\./g, '_')}.csv`), lines.join('\n'));
+}
+
+function expandLevel( specs, namespace, de, deFieldLines, depth, parentElementName ) {
+  if (depth >= 10) {
+    return;
+  }
+  let newDepth = depth + 1;
+  let parent_element = de.identifier.name;
+  //const humanRead = camelCaseToHumanReadable(`${de.identifier.name}.${name}`); 
+  //const deFieldLines = [];
+  let ms = ' ';
+  const valueAndFields = [de.value, ...de.fields];
+  for (let i=0; i < valueAndFields.length; i++) {
+    const f = valueAndFields[i];
+    //console.log(' newDepth=' + newDepth + ' cf=' + JSON.stringify(f));
+    if (f !== undefined) {
+      let nestedDE = specs._dataElements.findByIdentifier( f.identifier );
+      //console.log('\n ***element=' +  `"${namespace}","${parent_element}","${de.identifier.name}"` );
+      if( nestedDE !== undefined && nestedDE !== null ) {
+        if ( nestedDE._isEntry === true ) {
+          //console.log('\n newName=' + de.identifier.name + '\n');
+          //expandLevel( specs, namespace, nestedDE, deFieldLines, newDepth, parent_element + '.' + de.identifier.name ) ;
+          expandLevel( specs, namespace, nestedDE, deFieldLines, newDepth, de.identifier.name ) ;
+        }
+      }
+    }
+
+
+    let name = i>0 ? f.identifier.name : 'Value';
+    if (name === undefined || name === null) {
+      name = ' ';
+    }
+    let card = ' ';
+    if ( f !== undefined && f !== null && f.effectiveCard !== undefined && f.effectiveCard !== null
+      && f.effectiveCard.toString() !== undefined && f.effectiveCard.toString() !== null) {
+      card = f.effectiveCard.toString();
+    }
+    const fValueAndPath = dive(specs, f);
+    let dataType = getDataType(fValueAndPath);
+    dataType = removeValuePrefix(dataType);
+    const valueSet = getValueSet(f, fValueAndPath);
+    let descrip = '';
+    if (f !== undefined && f !== null) {
+      descrip = f.description;
+      descrip = getDescription( specs, f );
+      
+      if (descrip === undefined || descrip === null) {
+        descrip = de.description;
+      }
+      else {
+        descrip = descrip.replace(/["]+/g,'');
+      }
+    }
+    const humanRead = camelCaseToHumanReadable(`${de.identifier.name}.${name}`);
+    //deFieldLines.push(`"${namespace}","${parent_element}","${de.identifier.name}.${name}","${humanRead}","${descrip}",${card},"${dataType}","${valueSet}","${ms}"`);
+    deFieldLines.push(`"${namespace}","${parent_element}","${parentElementName}.${de.identifier.name}.${name}","${humanRead}","${descrip}",${card},"${dataType}","${valueSet}","${ms}"`);
+  }
+}
+
+
+
+
+function getSubElement(specs, field, path ) {
+  if (field !== undefined && field !== null ) {	
+    if (field.identifier) {
+      const fDef = specs.dataElements.findByIdentifier(field.effectiveIdentifier);
+      if (fDef && fDef.value && fDef.fields.length == 0) {
+        path.push(fDef.value.identifier ? fDef.value.identifier : new Identifier('', 'Value'));
+        return getSubElement(specs, fDef.value, path);
+      }
+    }
+    return { value: field, path };
+  }
+  return { value: '', path };
 }
 
 function dive(specs, field, path=[]) {
