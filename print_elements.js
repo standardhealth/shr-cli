7bfb9df47d9861129f8305f5e7a5784f4e714b67
Chris Moesel <cmoesel@mitre.org>
2019-04-09 12:07:06 -0400

    Merge constraints down to tail of path
    In order to collect all of the possible VS constraints anywhere in the path, merge all constraints down to the end.
    
    This heavily steals code from shr-fhir-export that should eventually be centralized to shr-models.  In addition, the code could probably be pruned more (may do more than we need) -- but for now... it works!

--- print_elements.js
+++ print_elements.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
-const { Identifier, IdentifiableValue, ChoiceValue } = require('shr-models');
-
-const ENTRY_ID = new Identifier('shr.base', 'Entry');
-const CONCEPT_ID = new Identifier('shr.core', 'CodeableConcept');
+const { IdentifiableValue, ChoiceValue, TypeConstraint, CardConstraint, IncludesTypeConstraint, ConstraintsFilter, RefValue } = require('shr-models');
 
 function getCard(f) {
   let card = ' ';
@@ -26,31 +23,179 @@ function getDataType(de) {
   return type;
 }
 
-function getBinding(de, path, dataElements, projectURL) {
+function getBinding(de, path, specs, projectURL) {
+  const value = findValueByPath(specs, path, de);
   let constraint;
-
-  if (typeof de.value !== 'undefined') {
-    constraint = de.value.constraintsFilter.valueSet.constraints[0];
-  } else if (typeof de.value === 'undefined' && path[0].isConceptKeyWord) {
-    constraint = de.value.constraintsFilter.valueSet.constraints[0];
+  const vsConstraints = value.constraintsFilter.valueSet.constraints;
+  if (vsConstraints && vsConstraints.length > 0) {
+    constraint = vsConstraints.find(c => !c.onValue && c.path.length === 0);
+    if (!constraint) {
+      constraint = vsConstraints.find(c => (c.onValue && c.path === 0) || (!c.onValue && c.path.length === 1));
+    }
   }
-
   if (constraint) {
     const url = constraint.valueSet.startsWith(projectURL) ? constraint.valueSet.split('/')[constraint.valueSet.split('/').length-1] : constraint.valueSet;
     return `${url} (${constraint.bindingStrength})`;
   }
+}
 
-  // We're not at the end of the path, so we must dig deeper
-  if (de && de.value) {
-    if (path.length > 0) {
-      de = dataElements.findByIdentifier(path[0]);
-      if (typeof def === 'undefined') {
-        return; // invalid path
+function choiceFriendlyEffectiveIdentifier(value) {
+  if (value.effectiveIdentifier) {
+    return value.effectiveIdentifier;
+  }
+  const ownTypeConstraints = value.constraintsFilter.own.type.constraints;
+  if (value instanceof ChoiceValue && ownTypeConstraints.length === 1) {
+    return ownTypeConstraints[0].isA;
+  }
+}
+
+// Given a path (identifier array) and a SHR data element definition, it will return the matching value at the tail
+// of the path with all constraints aggregrated onto it
+function findValueByPath(specs, path, def, valueOnly=false, parentConstraints=[]) {
+  if (path.length == 0) {
+    return;
+  }
+
+  const fieldsToSearch = [];
+  if (typeof def.value !== 'undefined') {
+    fieldsToSearch.push(mergeConstraintsToChild(parentConstraints, def.value, true));
+  }
+  if (!valueOnly) {
+    fieldsToSearch.push(...(def.fields.map(f => mergeConstraintsToChild(parentConstraints, f, false))));
+  }
+  // Find the value at the root of the path
+  let value = findValueByIdentifier(path[0], fieldsToSearch);
+
+  // If we didn't find the value, it could be one of those cases where we replaced the original identifier with
+  // an includesType identifier, so we should check the constraints to look for a match on the includesType.
+  if (typeof value === 'undefined' && parentConstraints.length > 0) {
+    const cf = new ConstraintsFilter(parentConstraints);
+    for (const itc of cf.includesType.constraints) {
+      if (itc.path.length == 1 && itc.isA.equals(path[0])) {
+
+        value = findValueByIdentifier(itc.path[0], fieldsToSearch);
+        if (typeof value !== 'undefined') {
+          if (value instanceof RefValue) {
+            value = new RefValue(itc.isA).withCard(itc.card).withConstraints(value.constraints);
+          } else {
+            value = new IdentifiableValue(itc.isA).withCard(itc.card).withConstraints(value.constraints);
+          }
+        }
       }
-      path = path.slice(1);
-      return getBinding(de, path, dataElements, projectURL);
     }
   }
+
+  if (typeof value === 'undefined') {
+    return; // invalid path
+  }
+
+  if (path.length == 1) {
+    return value; // this was the tail of the path
+  }
+
+  // We're not at the end of the path, so we must dig deeper
+  def = specs.dataElements.findByIdentifier(choiceFriendlyEffectiveIdentifier(value));
+  if (typeof def === 'undefined') {
+    return; // invalid path
+  }
+
+  // First see if we can continue the path by traversing the value
+  if (typeof def.value !== 'undefined') {
+    const subValue = findValueByPath(specs, path.slice(1), def, true, value.constraints);
+    if (typeof subValue !== 'undefined') {
+      return mergeConstraintsToChild(value.constraints, subValue, true);
+    }
+  }
+
+  // Still haven't found it, so traverse the rest
+  const subValue = findValueByPath(specs, path.slice(1), def, false, value.constraints);
+  if (typeof subValue !== 'undefined') {
+    return subValue;
+  }
+}
+
+// Given an identifier and a list of values, it will return the matching value, with all constraints aggregrated onto it
+function findValueByIdentifier(identifier, values) {
+  for (let value of values) {
+    if (value instanceof IdentifiableValue && value.possibleIdentifiers.some(pid => pid.equals(identifier))) {
+      // If the identifier isn't the value's direct identifier or effective identifier, it's
+      // probably from an includes type.  Check for that case.
+      if (!identifier.equals(value.identifier) && !identifier.equals(value.effectiveIdentifier)) {
+        for (const itc of value.constraintsFilter.includesType.constraints) {
+          if (itc.path.length == 0 && itc.isA.equals(identifier)) {
+            // It did resolve from an includes type, so return a value referencing the includes type instead!
+            // Remove any of the direct type-ish constraints and card constraints since we're setting type & card
+            const constraintsToCopy = value.constraints.filter(c => {
+              return c.path.length === 0
+                && !(c instanceof IncludesTypeConstraint)
+                && !(c instanceof TypeConstraint)
+                && !(c instanceof CardConstraint);
+            });
+            if (value instanceof RefValue) {
+              value = new RefValue(itc.isA).withCard(itc.card).withConstraints(constraintsToCopy);
+            } else {
+              value = new IdentifiableValue(itc.isA).withCard(itc.card).withConstraints(constraintsToCopy);
+            }
+            // Apply special marker used only in FHIR Exporter.  There is probably a more elegant way, but the
+            // alternative right now seems to require a ton of code
+            value._derivedFromIncludesTypeConstraint = true;
+            break;
+          }
+        }
+      }
+      return value;
+    } else if (value instanceof ChoiceValue) {
+      // First check to see if there is a type constraint to make this a single value type
+      const typeConstrained = value.constraintsFilter.own.type.constraints.some(c => c.isA.equals(identifier));
+
+      let opt = findValueByIdentifier(identifier, value.options);
+      if (typeof opt !== 'undefined') {
+        // We need to modify cardinality to:
+        // (a) use the choice's cardinality, because choice options are now ALWAYS 1..1
+        // (b) set min to 0 if there are multiple options (since it will have 0 instances if not selected)
+        opt = opt.clone().withCard(value.effectiveCard.clone());
+        if (value.options.length > 1 && !typeConstrained) {
+          opt.card.min = 0;
+        }
+        return mergeConstraintsToChild(value.constraints, opt);
+      }
+    }
+  }
+}
+
+function mergeConstraintsToChild(parentConstraints, childValue, childIsElementValue=false) {
+  let constraints = [];
+  for (const cst of parentConstraints) {
+    if (childIsElementValue && cst.path.length == 0 && cst.onValue) {
+      const transferredCst = cst.clone();
+      transferredCst.onValue = false;
+      constraints.push(transferredCst);
+    } else if (cst.path.length > 0 && (cst.path[0].equals(childValue.identifier) || cst.path[0].equals(choiceFriendlyEffectiveIdentifier(childValue)) )) {
+      const transferredCst = cst.clone();
+      transferredCst.path.shift(); // Remove the first element of the path since we're transferring this to the child
+      constraints.push(transferredCst);
+    }
+  }
+  // Remove any type constraints that are no-ops
+  constraints = constraints.filter(c => !(c instanceof TypeConstraint && c.isA.equals(choiceFriendlyEffectiveIdentifier(childValue))));
+  if (constraints.length == 0) {
+    return childValue;
+  }
+  const mergedChild = childValue.clone();
+  // Preserve special marker used only in FHIR Exporter.  There is probably a more elegant way, but the
+  // alternative right now seems to require a ton of code
+  if (childValue._derivedFromIncludesTypeConstraint) {
+    mergedChild._derivedFromIncludesTypeConstraint = true;
+  }
+  for (const cst of mergedChild.constraints) {
+    const siblings = new ConstraintsFilter(constraints).withPath(cst.path).constraints;
+    if (siblings.some(c => c.constructor.name == cst.constructor.name)) {
+      continue; // Don't add this constraint since the parent has the same type
+    }
+    constraints.push(cst);
+  }
+  mergedChild.constraints = constraints;
+  return mergedChild;
 }
 
 module.exports = function printElements(specs, config, out) {
@@ -60,7 +205,7 @@ module.exports = function printElements(specs, config, out) {
     const valueAndFields = [de.value, ...de.fields];
     for (const f of valueAndFields) {
       if (!f) continue; // no field
-      const cpRules = (specs.contentProfiles.findRulesByIdentifierAndField(de.identifier, f.identifier));      
+      const cpRules = (specs.contentProfiles.findRulesByIdentifierAndField(de.identifier, f.identifier));
       for (const rule of cpRules) {
         const parentName = de.identifier.name;
         const path = `${parentName}.${rule.path.map(id => id.name).join('.')}`;
@@ -69,7 +214,7 @@ module.exports = function printElements(specs, config, out) {
         const description = `"${endOfPathElement.description}"`;
         const cardinality = getCard(f);
         const dataType = getDataType(endOfPathElement);
-        const binding = getBinding(de, rule.path, specs.dataElements, config.projectURL);
+        const binding = getBinding(de, rule.path, specs, config.projectURL);
         lines.push([parentName, path, pathName, description, cardinality, dataType, binding].join(','));
       }
     }
